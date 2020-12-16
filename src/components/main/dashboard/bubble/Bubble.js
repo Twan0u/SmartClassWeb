1c877faf03f775982669fbc89d2d37a52a67c739
antoine951 <antoine_dumont@outlook.be>
2020-12-16 11:48:19 +0100

    Bubbles fonctionnent

--- src/components/main/dashboard/bubble/Bubble.js
+++ src/components/main/dashboard/bubble/Bubble.js
@@ -1,26 +1,70 @@
-import React from "react"
+import React, {Component} from "react"
 import Individual from "./Individual";
-function Bubble(props){
-    return (
-        <section className="info-tiles">
-            <div className="tile is-ancestor has-text-centered">
-                <Individual
-                    title = "42"
-                    apiCall = ""
-                    subtitle = "élèves"
-                />
-                <Individual
-                    title = "12"
-                    apiCall = ""
-                    subtitle = "Interrogations non signées"
-                />
-                <Individual
-                    title = "69"
-                    apiCall = ""
-                    subtitle = "Evénements à venir"
-                />
-            </div>
-        </section>
-    );
+import CreateAuthProvider from "../../../../libraries/createAuthProvider";
+
+class Bubble extends Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoading   : true,
+            numberEvents:0,
+            numberPupils:0,
+            numberTasks:0,
+        };
+    }
+
+    componentDidMount() {
+        const authProvider = CreateAuthProvider;
+        Promise.all([
+        fetch(authProvider.fetchApiURl('/events'), {
+            method :'get',
+            headers : authProvider.fetchHeaders(),
+            mode: 'cors',
+        }),
+        fetch(authProvider.fetchApiURl('/class/pupils'), {
+            method :'get',
+            headers : authProvider.fetchHeaders(),
+            mode: 'cors',
+        }),
+            fetch(authProvider.fetchApiURl('/tasks'), {
+                method :'get',
+                headers : authProvider.fetchHeaders(),
+                mode: 'cors',
+            })
+        ])
+            .then(([res1, res2, res3]) => Promise.all([res1.json(), res2.json(), res3.json()]))
+            .then(([res1, res2,  res3])=>{
+                this.setState({
+                    isLoading : false,
+                    numberEvents: res1.length,
+                    numberPupils: res2.length,
+                    numberTasks: res3.length,
+                });
+            })
+    }
+
+    componentWillUnmount() {}
+
+    render() {
+        return (
+            <section className="info-tiles">
+                <div className="tile is-ancestor has-text-centered">
+                    <Individual
+                        title = {(this.state.isLoading)?"Chargement":this.state.numberPupils}
+                        subtitle = "élève(s)"
+                    />
+                    <Individual
+                        title = {(this.state.isLoading)?"Chargement":this.state.numberTasks}
+                        subtitle = "tâche(s)"
+                    />
+                    <Individual
+                        title ={(this.state.isLoading)?"Chargement":this.state.numberEvents}
+                        subtitle = "événement(s)"
+                    />
+                </div>
+            </section>
+        );
+    }
 }
 export default Bubble;
