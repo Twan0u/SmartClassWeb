d7da072dc5400b8e99d8d41d118d621fe334c11a
antoine951 <antoine_dumont@outlook.be>
2021-01-14 22:47:34 +0100

    correction ortho et commentaires

--- src/components/main/dashboard/bubble/Bubble.js
+++ src/components/main/dashboard/bubble/Bubble.js
@@ -3,52 +3,58 @@ import Individual from "./Individual";
 
 import CreateAuthProvider from "../../../../libraries/createAuthProvider";
 
-class Bubble extends Component{
+class Bubble extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            isLoading   : true,
-            numberEvents:0,
-            numberPupils:0,
-            numberTasks:0,
+            isLoading: true,
+            numberEvents: 0,
+            numberPupils: 0,
+            numberTasks: 0,
         };
     }
 
     componentDidMount() {
         const authProvider = CreateAuthProvider;
         Promise.all([
-        fetch(authProvider.fetchApiURl('/events'), {
-            method :'get',
-            headers : authProvider.fetchHeaders(),
-            mode: 'cors',
-        }),
-        fetch(authProvider.fetchApiURl('/class/pupils'), {
-            method :'get',
-            headers : authProvider.fetchHeaders(),
-            mode: 'cors',
-        }),
+            fetch(authProvider.fetchApiURl('/events'), {
+                method: 'get',
+                headers: authProvider.fetchHeaders(),
+                mode: 'cors',
+            }),
+            fetch(authProvider.fetchApiURl('/class/pupils'), {
+                method: 'get',
+                headers: authProvider.fetchHeaders(),
+                mode: 'cors',
+            }),
             fetch(authProvider.fetchApiURl('/tasks'), {
-                method :'get',
-                headers : authProvider.fetchHeaders(),
+                method: 'get',
+                headers: authProvider.fetchHeaders(),
                 mode: 'cors',
             })
         ])
             .then(([res1, res2, res3]) => {
-                if (!res1.ok){throw res1}
-                if (!res2.ok){throw res2}
-                if (!res3.ok){throw res3}
+                if (!res1.ok) {
+                    throw res1
+                }
+                if (!res2.ok) {
+                    throw res2
+                }
+                if (!res3.ok) {
+                    throw res3
+                }
                 return Promise.all([res1.json(), res2.json(), res3.json()])
             })
-            .then(([res1, res2,  res3])=>{
+            .then(([res1, res2, res3]) => {
                 this.setState({
-                    isLoading : false,
+                    isLoading: false,
                     numberEvents: res1.length,
                     numberPupils: res2.length,
                     numberTasks: res3.length,
                 });
             })
-            .catch(function(error) {
+            .catch(function (error) {
                 switch (error.status) {
                     case 401:
                         alert("Il y a eu une erreur dans l'authentification de l'utilisateur");
@@ -62,31 +68,33 @@ class Bubble extends Component{
                     default:
                         alert("OOPS, il y a eu une erreur");
                 }
-                console.log("RESUME DE L'ERREUR : "+ error.message);
+                console.log("RESUME DE L'ERREUR : " + error.message);
             });
     }
 
-    componentWillUnmount() {}
+    componentWillUnmount() {
+    }
 
     render() {
         return (
             <section className="info-tiles">
                 <div className="tile is-ancestor has-text-centered">
                     <Individual
-                        title = {(this.state.isLoading)?"Chargement":this.state.numberPupils}
-                        subtitle = "élève(s)"
+                        title={(this.state.isLoading) ? "Chargement" : this.state.numberPupils}
+                        subtitle="élève(s)"
                     />
                     <Individual
-                        title = {(this.state.isLoading)?"Chargement":this.state.numberTasks}
-                        subtitle = "tâche(s)"
+                        title={(this.state.isLoading) ? "Chargement" : this.state.numberTasks}
+                        subtitle="tâche(s)"
                     />
                     <Individual
-                        title ={(this.state.isLoading)?"Chargement":this.state.numberEvents}
-                        subtitle = "événement(s)"
+                        title={(this.state.isLoading) ? "Chargement" : this.state.numberEvents}
+                        subtitle="événement(s)"
                     />
                 </div>
             </section>
         );
     }
 }
+
 export default Bubble;
